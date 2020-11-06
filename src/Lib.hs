97f0654b3779b77d46ab520ccfad7fc54b3cd5d5
Kohpai <whcwhc78@gmail.com>
2020-11-06 02:53:17 +0100

    :recycle: move variable to local

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,10 +10,9 @@ someFunc = putStrLn "someFunc"
 multiples3and5 :: Integer -> Integer
 multiples3and5 x = sum $ filter (\n -> n `mod` 3 == 0 || n `mod` 5 == 0) [1 .. (x - 1)]
 
-fibs :: [Integer]
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs) -- how and why does this work?
--- https://stackoverflow.com/questions/37243102/haskell-infinite-recursion
-
 -- limit
 evenFib :: Integer -> Integer
 evenFib x = sum $ takeWhile (<= x) $ filter even fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs) -- how and why does this work?
+    -- https://stackoverflow.com/questions/37243102/haskell-infinite-recursion
